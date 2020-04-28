e0cf2850ad6106eea11a2347bd251d700b205889
Johan <Johan.dahlstedt@hotmail.se>
2020-04-28 11:47:01 +0200

    Turn on water in the house using wrench

---
+++ Assets/Scripts/RepairSink.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairSink : MonoBehaviour
+{
+
+    public GameObject fakewater;
+
+    public TurnOnWater Water;
+
+    private bool coroutineAllowed;
+
+    void Start()
+    {
+        coroutineAllowed = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if (coroutineAllowed && Water.WaterOn > 0)
+        {
+            StartCoroutine("FixSink");
+        }
+
+        if (coroutineAllowed && Water.WaterOn == 0)
+        {
+            Debug.Log("Water In The House Seems To Be Turned Off.");
+        }
+    }
+
+    private IEnumerator FixSink()
+    {
+        coroutineAllowed = false;
+
+        transform.Rotate(0f, 0f, 30f);
+        yield return new WaitForSeconds(0.01f);
+
+        fakewater.SetActive(true);
+
+        coroutineAllowed = true;
+
+    }
+}
---
+++ Assets/Scripts/TurnOnWater.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnWater : MonoBehaviour
+{
+    public WrenchScript wrenchScript;
+
+    private bool coroutineAllowed;
+
+    public int WaterOn = 0;
+
+    void Start()
+    {
+        coroutineAllowed = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if (coroutineAllowed && wrenchScript.GotWrench > 0)
+        {
+            StartCoroutine("FixSink");
+        }
+
+        if (coroutineAllowed && wrenchScript.GotWrench == 0)
+        {
+            Debug.Log("You Need Some Object To Turn This Wheel");
+        }
+    }
+
+    private IEnumerator FixSink()
+    {
+        coroutineAllowed = false;
+
+        WaterOn = 1;
+
+        Debug.Log("Water Is Now Turned On");
+
+        transform.Rotate(0f, 60f, 0f);
+        yield return new WaitForSeconds(0.01f);
+
+        coroutineAllowed = true;
+    }
+}
