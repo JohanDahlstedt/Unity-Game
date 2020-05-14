9ae12aedf6680c83f44d4719f95cfea8909a715e
Johan <Johan.dahlstedt@hotmail.se>
2020-05-14 10:08:25 +0200

    Pick up letter with L

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -26,14 +26,41 @@ public class Raycast : MonoBehaviour
     public Transform start8;
     public Transform start9;
 
+    public Transform letterpos;
+
     bool carrying;
+    bool carryingPuzzle;
+
     GameObject holding;
+    public GameObject letter;
 
     public float interactionRayLength = 10.0f;
 
     void Update()
     {
         InteractRayCast();
+
+        if (Input.GetKeyDown(KeyCode.L) && !carrying && !carryingPuzzle)
+        {
+            letter.gameObject.transform.position = theDestination.position;
+            letter.gameObject.transform.rotation = theDestination.rotation;
+            letter.gameObject.transform.Rotate(0f, 90f, 90f);
+            letter.gameObject.transform.parent = GameObject.Find("Destination").transform;
+            letter.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            letter.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            StartCoroutine(waitswitch1());
+        }
+
+        if (Input.GetKeyDown(KeyCode.L) && carrying)
+        {
+            letter.gameObject.transform.position = letterpos.position;
+            letter.gameObject.transform.rotation = letterpos.rotation;
+            letter.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            letter.GetComponent<Rigidbody>().useGravity = true;
+            letter.GetComponent<BoxCollider>().enabled = true;
+            letter.gameObject.transform.parent = null;
+            StartCoroutine(waitswitch2());
+        }
     }
 
     void InteractRayCast()
@@ -45,15 +72,12 @@ public class Raycast : MonoBehaviour
         RaycastHit interactionRayHit;
 
         Vector3 interactionRayEndpoint = forwardDirection * interactionRayLength;
-        Debug.DrawLine(playerPosition, interactionRayEndpoint);
 
         bool hitFound = Physics.Raycast(interactionRay, out interactionRayHit, interactionRayLength);
         if (hitFound)
         {        
             GameObject hitGameobject = interactionRayHit.transform.gameObject;
             string hitFeedback = hitGameobject.name;
-
-            Debug.Log(hitFeedback);
 
             //Add Objects to Inventory
             if (hitGameobject.name == "Key1" && Input.GetKeyDown(KeyCode.E))
@@ -75,7 +99,7 @@ public class Raycast : MonoBehaviour
             }
 
             //Pickup Objects
-            if (hitGameobject.tag == "Puzzle" && Input.GetKeyDown(KeyCode.E) && !carrying)
+            if (hitGameobject.tag == "Puzzle" && Input.GetKeyDown(KeyCode.E) && !carrying && !carryingPuzzle)
             {
                 hitGameobject.gameObject.transform.position = theDestination.position;
                 hitGameobject.gameObject.transform.rotation = theDestination.rotation;
@@ -86,11 +110,11 @@ public class Raycast : MonoBehaviour
                 hitGameobject.gameObject.transform.gameObject.tag = "Holding";
                 hitGameobject.gameObject.layer = 2;
                 holding = GameObject.FindWithTag("Holding");
-                StartCoroutine(waitswitch1());
+                StartCoroutine(waitswitch3());
             }
 
             //Dropping each piece
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 1")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 1")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";    
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -100,9 +124,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot1.position;
                 holding.gameObject.transform.rotation = slot1.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 2")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 2")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -112,9 +136,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot2.position;
                 holding.gameObject.transform.rotation = slot2.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 3")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 3")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -124,9 +148,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot3.position;
                 holding.gameObject.transform.rotation = slot3.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 4")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 4")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -136,9 +160,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot4.position;
                 holding.gameObject.transform.rotation = slot4.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 5")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 5")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -148,9 +172,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot5.position;
                 holding.gameObject.transform.rotation = slot5.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 6")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 6")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -160,9 +184,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot6.position;
                 holding.gameObject.transform.rotation = slot6.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 7")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 7")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -172,9 +196,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot7.position;
                 holding.gameObject.transform.rotation = slot7.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 8")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 8")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -184,9 +208,9 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot8.position;
                 holding.gameObject.transform.rotation = slot8.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.name == "Slot 9")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.name == "Slot 9")
             {
                 holding.gameObject.transform.gameObject.tag = "Puzzle";
                 holding.gameObject.GetComponent<Rigidbody>().isKinematic = false;
@@ -196,10 +220,10 @@ public class Raycast : MonoBehaviour
                 holding.gameObject.layer = 0;
                 holding.gameObject.transform.position = slot9.position;
                 holding.gameObject.transform.rotation = slot9.rotation;
-                StartCoroutine(waitswitch2());
+                StartCoroutine(waitswitch4());
             }
 
-            if (Input.GetKeyDown(KeyCode.E) && carrying && hitGameobject.tag != "Slot")
+            if (Input.GetKeyDown(KeyCode.E) && carryingPuzzle && hitGameobject.tag != "Slot")
             {
                 if (holding.gameObject.name == "Piece 1")
                 {
@@ -211,7 +235,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 2")
                 {
@@ -223,7 +247,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 3")
                 {
@@ -235,7 +259,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 4")
                 {
@@ -247,7 +271,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 5")
                 {
@@ -259,7 +283,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 6")
                 {
@@ -271,7 +295,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 7")
                 {
@@ -283,7 +307,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 8")
                 {
@@ -295,7 +319,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
                 if (holding.gameObject.name == "Piece 9")
                 {
@@ -307,7 +331,7 @@ public class Raycast : MonoBehaviour
                     holding.GetComponent<BoxCollider>().enabled = true;
                     holding.gameObject.transform.parent = null;
                     holding.gameObject.layer = 0;
-                    StartCoroutine(waitswitch2());
+                    StartCoroutine(waitswitch4());
                 }
             }
         }
@@ -315,13 +339,23 @@ public class Raycast : MonoBehaviour
 
     IEnumerator waitswitch1()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.01f);
         carrying = true;
     }
 
     IEnumerator waitswitch2()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.01f);
         carrying = false;
+    }
+    IEnumerator waitswitch3()
+    {
+        yield return new WaitForSeconds(0.01f);
+        carryingPuzzle = true;
+    }
+    IEnumerator waitswitch4()
+    {
+        yield return new WaitForSeconds(0.01f);
+        carryingPuzzle = false;
     }
 }
