1a0d40aa9a0a54cf61d260989c19606b3ac63bf8
Johan <Johan.dahlstedt@hotmail.se>
2020-05-28 13:07:35 +0200

    push

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -8,8 +8,6 @@ public class Raycast : MonoBehaviour
     public Inventory inventory;
     public Transform theDestination;
 
-    public float speed;
-
     public Transform slot1;
     public Transform slot2;
     public Transform slot3;
